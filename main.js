9ca1d89cbfc52a9055a3a111ee5effe95a479f25
Kayfresh <madubuike1573080@gmail.com>
2024-11-15 13:00:16 +0100

    Added recommendations, more results, and error if no gendr selected, also ability to deselect gender

--- main.js
+++ main.js
@@ -14,23 +14,54 @@ const calculate = document.getElementById("calculate");
 
 const BMIAnswer = document.getElementById("bmi-ans");
 const result = document.getElementById("health");
+const recommendation = document.getElementById("recommend");
+
+let maleselected = false;
+let femaleselected = false;
+
+const resetGender = () => {
+  maleIcon.style.display = "none";
+  femaleIcon.style.display = "none";
+  personIcon.style.display = "block";
+  male.style.backgroundColor = "#dceffe";
+  female.style.backgroundColor = "#dceffe";
+  maleselected = false;
+ femaleselected = false;
+};
 
 male.addEventListener("click", () => {
-  maleIcon.style.display = "block";
-  femaleIcon.style.display = "none";
-  personIcon.style.display = "none";
+  if (maleselected === false) {
+    resetGender();
 
-  male.style.backgroundColor = "#299cfb";
-  female.style.backgroundColor = "#ebebeb";
+    maleIcon.style.display = "block";
+    femaleIcon.style.display = "none";
+    personIcon.style.display = "none";
+
+    male.style.backgroundColor = "#299cfb";
+    female.style.backgroundColor = "#ebebeb";
+
+    maleselected = true;
+    femaleselected = false;
+  } else {
+    resetGender();
+  }
 });
 
 female.addEventListener("click", () => {
-  maleIcon.style.display = "none";
-  femaleIcon.style.display = "block";
-  personIcon.style.display = "none";
+  if (femaleselected === false) {
 
-  female.style.backgroundColor = "#299cfb";
-  male.style.backgroundColor = "#ebebeb";
+    maleIcon.style.display = "none";
+    femaleIcon.style.display = "block";
+    personIcon.style.display = "none";
+
+    female.style.backgroundColor = "#299cfb";
+    male.style.backgroundColor = "#ebebeb";
+
+    femaleselected = true;
+    maleselected = false;
+  } else {
+    resetGender();
+  }
 });
 
 const emptyError = () => {
@@ -42,21 +73,27 @@ const NanError = () => {
 };
 
 const runCalculations = () => {
-  const ageValue = parseFloat(age.value);
+  const ageValue = parseInt(age.value);
   const weightValue = parseFloat(weight.value);
   const heightValue = parseFloat(height.value);
 
+  
   if (weight.Value === "" || height.Value === "" || age.value === "") {
     emptyError();
     calculate.style.backgroundColor = "#299cfb";
     calculate.style.color = "#fff";
     return;
   }
-
+  
   if (isNaN(weightValue) || isNaN(heightValue) || isNaN(ageValue)) {
     NanError();
     calculate.style.backgroundColor = "#299cfb";
     calculate.style.color = "#fff";
+    return;
+  }
+  
+  if (!maleselected && !femaleselected ) {
+    alert ("Please select a Gender");
     return;
   }
 
@@ -69,22 +106,37 @@ const runCalculations = () => {
     if (bmi < 18.5) {
       result.textContent = "Underweight";
       result.style.color = "blue";
-    } else if (bmi >= 18.5 && bmi < 24.9) {
+      recommendation.textContent = "Eat more and exercise more.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
       result.textContent = "Normal weight";
       result.style.color = "green";
-    } else if (bmi >= 25 && bmi < 29.9) {
-      result.textContent = "Overweight";
-      result.style.color = "orange";
+      recommendation.textContent = "Well done, Maintain a healthy lifestyle";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      result.textContent = "Overweight but not Obese";
+      result.style.color = "lime";
+      recommendation.textContent = "Reduce food amount and work out more";
+    } else if (bmi >= 30 && bmi <= 34.9) {
+      result.textContent = "Obese Class I";
+      reset.style.color = "yellow";
+      recommendation.textContent =
+        "Work out even more and reduce fatty foods and amount of food eaten daily";
+    } else if (bmi >= 35 && bmi <= 39.9) {
+      result.textContent = "Obese Class II";
+      reset.style.color = "Orange";
+      recommendation.textContent =
+        "You are on a dangerous path, get a gym membership and eat healthy food";
     } else {
-      result.textContent = "Obesity";
+      result.textContent = "Obese Class III (Severe Obesity)";
       result.style.color = "red";
+      recommendation.textContent =
+        "You are at a risk of having a Heart attack, stop fatty foods and see a Physician";
     }
   }
 };
 
 calculate.addEventListener("click", (event) => {
-  calculate.style.backgroundColor = "#fff";
-  calculate.style.color = "#299cfb";
+  // calculate.style.backgroundColor = "#fff";
+  // calculate.style.color = "#299cfb";
   event.preventDefault();
   runCalculations();
 });
@@ -94,11 +146,12 @@ reset.addEventListener("click", () => {
   personIcon.style.display = "block";
   maleIcon.style.display = "none";
   femaleIcon.style.display = "none";
-  female.style.backgroundColor = "#ebebeb";
-  male.style.backgroundColor = "#ebebeb";
+  female.style.backgroundColor = "#dceffe";
+  male.style.backgroundColor = "#dceffe";
   weight.value = "";
   height.value = "";
   BMIAnswer.textContent = "0.00";
   result.textContent = "-----";
   result.style.color = "#000";
+  recommendation.textContent = ""
 });
